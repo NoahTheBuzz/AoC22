8ff07b31c5e40ccab05e5202e30f895ff55df2f5
Noah <noah.sarcevic@online.de>
2022-12-19 14:45:28 +0100

    feat: added the text files for task 7-10 and implemented part A of task 6

---
+++ AdventOfCode/src/Day6/Day6.java
@@ -0,0 +1,59 @@
+package Day6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+        partA();
+        partB();
+    }
+
+    private static void partA(){
+        try{
+            Scanner sc = new Scanner(new File("src/Day6/input.txt"));
+
+            String input = "";
+
+            if(sc.hasNext()){
+                input = sc.next();
+            }
+
+            char[] arr = new char[4];
+            int marker = 0;
+
+            while(marker + 4 < input.length()) {
+                for (int i = marker; i < arr.length + marker; i++) {
+                    arr[i - marker] = input.charAt(i);
+                }
+
+                if (checkForMarker(arr)) {
+                    marker = marker + 4;
+                    break;
+                }
+                System.out.println("Current marker: " + marker);
+                marker++;
+            }
+
+            System.out.println("Marker at: " + marker);
+
+        }catch (FileNotFoundException ignored){}
+    }
+
+    private static void partB(){
+
+    }
+
+    private static boolean checkForMarker(char[] input){
+        for(int i = 0; i < input.length-1; i++){
+            for(int n = i+1; n < input.length; n++){
+                if(input[i] == input[n]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ AdventOfCode/src/Day7/Day7.java
@@ -0,0 +1,4 @@
+package Day7;
+
+public class Day7 {
+}
